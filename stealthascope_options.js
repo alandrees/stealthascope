2976bfbaa9d384c0986d9cc5acfd29a2f0620c06
Alan Drees <alandrees@theselves.com>
2014-09-08 14:48:01 -0400

    added stealthascope_options.js to the index

---
+++ stealthascope_options.js
@@ -0,0 +1,11 @@
+/**
+ * Copyright 2014 Alan Drees
+ *   
+ * Purpose:
+ *   Defines options for the Stealthascope controllerb
+ */
+var Stlh = Stlh || {};
+
+Stlh.options = {'inputs' : [2,2],
+		'bcrfs'  : 1,
+		'lps'    : 1};
