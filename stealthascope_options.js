92ba2cd8319f90bdab93de573d3861c3eec6c309
Alan Drees <alandrees@theselves.com>
2014-09-10 10:09:21 -0400

    added tracks/scenes/sends options

--- stealthascope_options.js
+++ stealthascope_options.js
@@ -2,10 +2,13 @@
  * Copyright 2014 Alan Drees
  *   
  * Purpose:
- *   Defines options for the Stealthascope controllerb
+ *   Defines options for the Stealthascope controller
  */
 var Stlh = Stlh || {};
 
 Stlh.options = {'interfaces' : [2,2],
 		'bcrfs'      : 1,
-		'lps'        : 1};
+		'lps'        : 1,
+		'tracks'     : 8,
+	        'scenes'     : 8,
+	        'sends'      : 3};
