0d6e3b1a222b3dfcda5c3363dfc5db7d3a855e00
Alan Drees <alandrees@theselves.com>
2015-06-14 10:17:59 -0400

    updated the Stlh.options object to support two launchpads

--- stealthascope_options.js
+++ stealthascope_options.js
@@ -16,3 +16,12 @@ Stlh.options = {'interfaces' : [2,3],
 	        'scenes'     : 8,
 	        'sends'      : 3};
 */
+
+Stlh.options = {'interfaces' : [3,4],
+		'bcfrs'      : 1,
+		'lps'        : 2,
+		'cds'        : 1,
+		'tracks'     : 8,
+		'channels'   : 8,
+	        'scenes'     : 16,
+	        'sends'      : 3};
